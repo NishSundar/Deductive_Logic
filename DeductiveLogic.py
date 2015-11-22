2ecbf6084f59fd37a38a457f58e4eaeda486bd36
Nishant Sundar <nish.sundar@gmail.com>
2015-11-22 17:04:09 +0530

    Made some changes to the storing of the boolean values.

--- DeductiveLogic.py
+++ DeductiveLogic.py
@@ -26,61 +26,73 @@ def compute(read_string):
 	# Do the True, False values first
 	for i in boolean_dict.keys():
 		if boolean_dict[i] == ['True']:
-			boolean_dict[i] = [True]
+			boolean_dict[i] = True
 		elif boolean_dict[i] == ['False']:
-			boolean_dict[i] = [False]
+			boolean_dict[i] = False
 
 	# Put in the values of independent variables into the dependent variables first
 	for i in boolean_dict.keys():
 		y = boolean_dict[i]
-		for j in range(0, len(y)):
-			if y[j] in boolean_dict.keys():
-				k = boolean_dict[y[j]]
-				if k == [True] or k == [False]:
-					y[j] = k
-				#else:
-					#print "This is not a valid set of variables."
-					#exit(-1)
+		if y == True or y == False:
+			pass
+		else:
+			for j in range(0, len(y)):
+				if y[j] in boolean_dict.keys():
+					k = boolean_dict[y[j]]
+					if k == True or k == False:
+						y[j] = k
+					#else:
+						#print "This is not a valid set of variables."
+						#exit(-1)
 		
 	# Do the not operations second
 	for i in boolean_dict.keys():
 		y = boolean_dict[i]
-		for j in range(0, len(y)):
-			if y[j] == 'not':
-				z = y[j+1]
-				if z == True or z == False:
-					y[j:j+2] = [not z]
-				#else:
-					#print "This is not a valid set of variables."
-					#exit(-1)
+		if y == True or y == False:
+			pass
+		else:
+			for j in range(0, len(y)):
+				if y[j] == 'not':
+					z = y[j+1]
+					if z == True or z == False:
+						y[j:j+2] = [not z]
+					#else:
+						#print "This is not a valid set of variables."
+						#exit(-1)
 	
 	# Do the and operations next
 	for i in boolean_dict.keys():
 		y = boolean_dict[i]
-		for j in range(0, len(y)):
-			if y[j] == 'and':
-				l = y[j-1]
-				m = y[j+1]
-				if (l == True or l == False) and (m == True or m == False):
-					z = l and m
-					y[j-1:j+2] = [z]
-				#else:
-					#print "This is not a valid set of variables."
-					#exit(-1)
+		if y == True or y == False:
+			pass
+		else:
+			for j in range(0, len(y)):
+				if y[j] == 'and':
+					l = y[j-1]
+					m = y[j+1]
+					if (l == True or l == False) and (m == True or m == False):
+						z = l and m
+						y[j-1:j+2] = [z]
+					#else:
+						#print "This is not a valid set of variables."
+						#exit(-1)
 	
 	# Do the or operations last
 	for i in boolean_dict.keys():
 		y = boolean_dict[i]
-		for j in range(0, len(y)):
-			if y[j] == 'or':
-				l = y[j-1]
-				m = y[j+1]
-				if (l == True or l == False) and (m == True or m == False):
-					z = l or m
-					y[j-1:j+2] = [z]
-				#else:
-					#print "This is not a valid set of variables."
-					#exit(-1)
+		if y == True or y == False:
+			pass
+		else:
+			for j in range(0, len(y)):
+				if y[j] == 'or':
+					l = y[j-1]
+					m = y[j+1]
+					if (l == True or l == False) and (m == True or m == False):
+						z = l or m
+						y[j-1:j+2] = [z]
+					#else:
+						#print "This is not a valid set of variables."
+						#exit(-1)
 					
 	for i in range(0, len(equation)):
 		if equation[i] in boolean_dict.keys():
