91d8cf1f51c0c65eccb41be836e55f555e222ae9
Nishant Sundar <nish.sundar@gmail.com>
2015-11-24 14:37:04 +0530

    Works for simple examples (without parenthesis and multiple logic statements) like TestExample

--- DeductiveLogic.py
+++ DeductiveLogic.py
@@ -26,21 +26,23 @@ def compute(read_string):
 	# Do the True, False values first
 	for i in boolean_dict.keys():
 		if boolean_dict[i] == ['True']:
-			boolean_dict[i] = True
+			boolean_dict[i] = [True]
 		elif boolean_dict[i] == ['False']:
-			boolean_dict[i] = False
+			boolean_dict[i] = [False]
 
 	# Put in the values of independent variables into the dependent variables first
 	for i in boolean_dict.keys():
 		y = boolean_dict[i]
-		if y == True or y == False:
-			pass
-		else:
-			for j in range(0, len(y)):
-				if y[j] in boolean_dict.keys():
-					k = boolean_dict[y[j]]
-					if k == True or k == False:
-						y[j] = k
+		l = len(y)
+		if y != [True] or y != [False]:
+			for j in range(0, l):
+				if j >= len(y):
+					break
+				x = y[j]
+				if x in boolean_dict.keys():
+					k = boolean_dict[x]
+					if k == [True] or k == [False]:
+						y[j] = k[0]
 					#else:
 						#print "This is not a valid set of variables."
 						#exit(-1)
@@ -48,11 +50,13 @@ def compute(read_string):
 	# Do the not operations second
 	for i in boolean_dict.keys():
 		y = boolean_dict[i]
-		if y == True or y == False:
-			pass
-		else:
-			for j in range(0, len(y)):
-				if y[j] == 'not':
+		l = len(y)
+		if y != [True] or y != [False]:
+			for j in range(0, l):
+				if j >= len(y):
+					break
+				x = y[j]
+				if x == 'not':
 					z = y[j+1]
 					if z == True or z == False:
 						y[j:j+2] = [not z]
@@ -63,15 +67,17 @@ def compute(read_string):
 	# Do the and operations next
 	for i in boolean_dict.keys():
 		y = boolean_dict[i]
-		if y == True or y == False:
-			pass
-		else:
-			for j in range(0, len(y)):
-				if y[j] == 'and':
-					l = y[j-1]
-					m = y[j+1]
-					if (l == True or l == False) and (m == True or m == False):
-						z = l and m
+		l = len(y)
+		if y != [True] or y != [False]:
+			for j in range(0, l):
+				if j >= len(y):
+					break
+				x = y[j]
+				if x == 'and':
+					m = y[j-1]
+					n = y[j+1]
+					if (m == True or m == False) and (n == True or n == False):
+						z = m and n
 						y[j-1:j+2] = [z]
 					#else:
 						#print "This is not a valid set of variables."
@@ -80,35 +86,51 @@ def compute(read_string):
 	# Do the or operations last
 	for i in boolean_dict.keys():
 		y = boolean_dict[i]
-		if y == True or y == False:
-			pass
-		else:
-			for j in range(0, len(y)):
-				if y[j] == 'or':
-					l = y[j-1]
-					m = y[j+1]
-					if (l == True or l == False) and (m == True or m == False):
-						z = l or m
+		l = len(y)
+		if y != [True] or y != [False]:
+			for j in range(0, l):
+				if j >= len(y):
+					break
+				x = y[j]
+				if x == 'or':
+					m = y[j-1]
+					n = y[j+1]
+					if (m == True or m == False) and (n == True or n == False):
+						z = m or n
 						y[j-1:j+2] = [z]
 					#else:
 						#print "This is not a valid set of variables."
 						#exit(-1)
-					
-	for i in range(0, len(equation)):
-		if equation[i] in boolean_dict.keys():
-			equation[i] = boolean_dict[equation[i]]
-	for i in range(0, len(equation)):
-		if equation[i] == 'not':
+	
+	l = len(equation)
+	for i in range(0, l):
+		if i >= len(equation):
+					break
+		x = equation[i]
+		if x in boolean_dict.keys():
+			equation[i] = boolean_dict[equation[i]][0]
+	for i in range(0, l):
+		if i >= len(equation):
+			break
+		x = equation[i]
+		if x == 'not':
 			equation[i:i+2] = [not equation[i+1]]
-	for i in range(0, len(equation)):
-		if equation[i] == 'and':
+	for i in range(0, l):
+		if i >= len(equation):
+			break
+		x = equation[i]
+		if x == 'and':
 			equation[i-1:i+2] = [equation[i-1] and equation[i+1]]
-	for i in range(0, len(equation)):
-		if equation[i] == 'or':
+	for i in range(0, l):
+		if i >= len(equation):
+			break	
+		x = equation[i]
+		if x == 'or':
 			equation[i-1:i+2] = [equation[i-1] and equation[i+1]]
 	
 	solution = equation[0]
 	print boolean_dict
+	print equation
 	
 	read.close()
 	return solution, equation_name
