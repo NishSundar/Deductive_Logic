24f569da7b6c688a3d0b28dc4733794c509804e0
nanonaren <nano.naren@gmx.com>
2015-11-25 14:00:10 +0530

    Adding a test script

---
+++ test.py
@@ -0,0 +1,37 @@
+import subprocess
+
+def writeTest(str):
+    f = open("TestFile","w")
+    f.write(str)
+    f.close()
+
+def run():
+    p = subprocess.Popen(["python2", "DeductiveLogic.py", "TestFile"], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    return out.splitlines()[-1].split()[-1]
+
+def runTest(name,expected,str):
+    writeTest(str)
+    actual = run()
+    status = "failed!"
+    if actual == expected:
+        status = "passed!"
+    
+    output = "Test '%s' %s" % (name,status)
+    print output
+
+############################################################
+# TESTS
+############################################################
+
+runTest("OR-1",True,"""Variable A True
+Variable B False
+Equation Test A or B""")
+
+runTest("AND-1",True,"""Variable A True
+Variable B True
+Equation Test A and B""")
+
+runTest("AND-2",False,"""Variable A True
+Variable B False
+Equation Test A and B""")
